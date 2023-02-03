be9dd8266cf89d35ca1f9b95e36bf319f9c76dc3
saulpountney <54355533+saulpountney@users.noreply.github.com>
2023-02-03 13:28:03 +0000

    added spock and lizard

--- .idea/programs/games/RPC.py
+++ .idea/programs/games/RPC.py
@@ -8,18 +8,25 @@ while True:
     print("...rock...")
     print("...paper...")
     print("...scissors...")
+    print("...lizard...")
+    print("...spock...")
 
     #Player is prompted for choice.
     player = input("(Enter your choice): ").lower()
     #Option to quit.
     if player == "quit" or player == "q":
         break
+
     #Computer choice
-    random_num = random.randint(0,2)
+    random_num = random.randint(0,4)
     if random_num == 0:
         computer = "rock"
     elif random_num == 1:
         computer = "paper"
+    elif random_num == 2:
+        computer = "lizard"
+    elif random_num == 3:
+        computer = "spock"
     else:
         computer = "scissors"
 
@@ -32,22 +39,55 @@ while True:
         if computer == "paper":
             print("Computer wins!")
             computerScore += 1
-        else:
-            print("player wins!")
-            playerScore += 1
-    elif player == "paper" or player == "p":
-        if computer == "rock":
-            print("player wins!")
-            playerScore += 1
-        else:
+        elif computer == "spock":
             print("Computer wins!")
             computerScore += 1
+        else:
+            print("player wins!")
+            playerScore += 1
+
+    elif player == "paper" or player == "p":
+        if computer == "sissors":
+            print("Computer wins!")
+            computerScore += 1
+        elif computer == "lizard":
+            print("Computer wins!")
+            computerScore += 1
+        else:
+            print("player wins!")
+            playerScore += 1
+
     elif player == "scissors" or player == "s":
         if computer == "rock":
             print("Computer wins!")
             computerScore += 1
+        elif computer == "spock":
+            print("Computer wins!")
+            computerScore += 1
         else:
-            print("Player wins!")
+            print("player wins!")
+            playerScore += 1
+
+    elif player == "lizard" or player == "l":
+        if computer == "rock":
+            print("Computer wins!")
+            computerScore += 1
+        elif computer == "lizard":
+            print("Computer wins!")
+            computerScore += 1
+        else:
+            print("player wins!")
+            playerScore += 1
+
+    elif player == "spock" or player == "v":
+        if computer == "paper":
+            print("Computer wins!")
+            computerScore += 1
+        elif computer == "lizard":
+            print("Computer wins!")
+            computerScore += 1
+        else:
+            print("player wins!")
             playerScore += 1
 
     #Error Correction
